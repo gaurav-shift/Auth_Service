aacc8cc100dac069202122975c4f35e79fc4b360
gaurav-shift <agarwalgaurav302005@gmail.com>
2026-03-08 19:14:20 +0530

    Added user isAuth function

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -14,5 +14,10 @@ router.post(
     AuthRequestValidators.validateUserAuth,
     UserController.signIn);
 
+router.get(
+    './isAuthenticated',
+    UserController.isAuthenticated
+)
+
 module.exports = router;
 
