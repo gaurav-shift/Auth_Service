3a965178fd8d37534793ec6a7bb76b9958ee89c5
gaurav-shift <agarwalgaurav302005@gmail.com>
2026-03-09 23:12:07 +0530

    Added roles and user roles through table

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const bodyParser = require('body-parser');
 const { PORT } = require('./config/serverConfig');
 const apiRoutes = require('./routes/index');
 
+const db = require('./models/index');
+
 const app = express();
 
 const prepareAndStartServer = () => {
@@ -12,8 +14,13 @@ const prepareAndStartServer = () => {
     app.use(bodyParser.urlencoded({extended: true}));
 
     app.use('/api',apiRoutes);
-    app.listen(PORT, ()=>{
+    app.listen(PORT, async ()=>{
         console.log(`Server Started on PORT: ${PORT}`);
+        if(process.env.DB_SYNC){
+            db.sequelize.sync({alter: true});
+        }
+
+
     })
 }
 
--- src/services/user-service.js
+++ src/services/user-service.js
@@ -44,7 +44,7 @@ class UserService{
             if(!response){
                 throw {error: 'Invalid Token'}
             }
-            const user = this.UserRepository.getById(response.id);
+            const user = await this.UserRepository.getById(response.id);
             if(!user){
                 throw {error : 'No user with the corresponding token exists'};
             }
